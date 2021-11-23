3a970a0f4abd54e34b8a5c26f52f2c55247f8a06
Facundo-Martiarena <53951865+Facundo-Martiarena@users.noreply.github.com>
2021-11-23 19:25:56 -0300

    Add files via upload

---
+++ src/ItemDetailContainer/ItemDetail.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Item from './../components/Item';
+
+const ItemDetail = ({product}) => {
+    return (
+        <>
+            <Item product={product} />
+        </>
+    )
+}
+
+export default ItemDetail
